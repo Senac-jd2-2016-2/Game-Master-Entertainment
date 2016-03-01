a478d9828b460aae0e404afa8a28f49422736791
FigueiredoxXx <gugustavo.lima@hotmail.com>
2016-03-01 04:00:03 -0300

    test d novo!

--- Game1/Game1.cs
+++ Game1/Game1.cs
@@ -14,6 +14,7 @@ namespace Game1
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
         }
 
         protected override void Initialize()
